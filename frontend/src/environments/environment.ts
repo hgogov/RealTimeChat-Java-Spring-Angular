5c419406cbaf7b5944b194b829829ca3b8ee3ee1
hgogov <hristian098@abv.bg>
2025-04-03 12:47:28 +0300

    chore(frontend): Align development environment API URL for proxy

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8080',
-  wsUrl: 'http://localhost:8080/chat-websocket'
+  apiUrl: '',
+  wsUrl: 'ws://localhost:8080/ws'
 };
